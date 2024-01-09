5046bba56dd1789cf1a0b7863fc1778ef3806454
Alesson Calaça <alesson.calassa@gmail.com>
2024-01-09 19:37:01 -0300

    Programa concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Realizando os testes com as classes Nokia e Iphone
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "A155", imei: "123555", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("-------------------------------------------------");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "123789", modelo: "SE2", imei: "789789", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+Console.WriteLine("-------------------------------------------------");
